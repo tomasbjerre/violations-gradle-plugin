2204fe5dc29ec120f4a5e2d824ffbab0ba33b438
Tomas Bjerre <tomas.bjerre85@gmail.com>
2026-09-13 08:06:51 +0200

    feat: replacing gson with jackson

--- src/main/java/se/bjurr/violations/gradle/plugin/ViolationsTask.java
+++ src/main/java/se/bjurr/violations/gradle/plugin/ViolationsTask.java
@@ -33,7 +33,7 @@ import se.bjurr.violations.lib.model.SEVERITY;
 import se.bjurr.violations.lib.model.Violation;
 import se.bjurr.violations.lib.reports.Parser;
 import se.bjurr.violations.lib.util.Filtering;
-import se.bjurr.violations.violationslib.com.google.gson.GsonBuilder;
+import tools.jackson.databind.ObjectMapper;
 
 public class ViolationsTask extends DefaultTask {
 
@@ -174,7 +174,8 @@ public class ViolationsTask extends DefaultTask {
   }
 
   private void createJsonFile(final Object object, final File file) throws IOException {
-    final String codeClimateReport = new GsonBuilder().setPrettyPrinting().create().toJson(object);
+    final String codeClimateReport =
+        new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(object);
     final Path path = file.toPath();
     path.toFile().getParentFile().mkdirs();
     Files.write(
